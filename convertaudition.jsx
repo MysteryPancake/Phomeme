9d85045d41ca477c1ba39e08fdcd677df7a3ec95
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-04-05 20:58:36 +1000

    video layers work now (annoying solution)

--- convertaudition.jsx
+++ convertaudition.jsx
@@ -37,6 +37,28 @@
 		return [left, right];
 	}
 
+	function addClips(list, comp, files, rate) {
+		for each (var clip in list) {
+			var layer = comp.layers.add(files[clip.@fileID.toString()]);
+			layer.name = clip.@name.toString();
+			layer.stretch = (Number(clip.clipStretch.@stretchRatio) || 1) * 100;
+			var startTime = Number(clip.@sourceInPoint) / rate;
+			var inPoint = Number(clip.@startPoint) / rate;
+			var outPoint = Number(clip.@endPoint) / rate;
+			layer.startTime = inPoint - startTime;
+			layer.inPoint = inPoint;
+			layer.outPoint = outPoint;
+			layer.locked = clip.@lockedInTime.toString() === "true";
+			layer.audioEnabled = clip.component.(@name == "Mute").parameter.(@index == "1").@parameterValue.toString() === "0";
+			var pan = Number(clip.component.(@name == "StereoPanner").parameter.(@name == "Pan").@parameterValue);
+			var volume = Number(clip.component.(@name == "volume").parameter.(@name == "volume").@parameterValue);
+			var gain = Number(clip.component.(@name == "volume").parameter.(@name == "static gain").@parameterValue);
+			if (layer.hasAudio) {
+				layer.audio.audioLevels.setValue(panVolume(pan, volume * gain));
+			}
+		}
+	}
+
 	function parse(xml, session) {
 		var master;
 		var comps = [];
@@ -55,26 +77,8 @@
 			var comp = app.project.items.addComp(prop.trackParameters.name.toString(), 1920, 1080, 1, duration / rate, 60);
 			comp.time = Number(xml.session.sessionState.@ctiPosition) / rate;
 			comp.parentFolder = folder;
-			for each (var clip in prop..audioClip) {
-				var file = clip.@fileID.toString();
-				var layer = comp.layers.add(files[file]);
-				layer.name = clip.@name.toString();
-				layer.stretch = (Number(clip.clipStretch.@stretchRatio) || 1) * 100;
-				var startTime = Number(clip.@sourceInPoint) / rate;
-				var inPoint = Number(clip.@startPoint) / rate;
-				var outPoint = Number(clip.@endPoint) / rate;
-				layer.startTime = inPoint - startTime;
-				layer.inPoint = inPoint;
-				layer.outPoint = outPoint;
-				layer.locked = clip.@lockedInTime.toString() === "true";
-				layer.audioEnabled = clip.component.(@name == "Mute").parameter.(@index == "1").@parameterValue.toString() === "0";
-				var pan = Number(clip.component.(@name == "StereoPanner").parameter.(@name == "Pan").@parameterValue);
-				var volume = Number(clip.component.(@name == "volume").parameter.(@name == "volume").@parameterValue);
-				var gain = Number(clip.component.(@name == "volume").parameter.(@name == "static gain").@parameterValue);
-				if (layer.hasAudio) {
-					layer.audio.audioLevels.setValue(panVolume(pan, volume * gain));
-				}
-			}
+			addClips(prop..audioClip, comp, files, rate);
+			addClips(prop..videoClip, comp, files, rate);
 			if (prop.name() == "masterTrack") {
 				master = comp;
 			} else {
@@ -92,7 +96,6 @@
 			for (var i = 0; i < comps.length; i++) {
 				var comp = master.layers.add(comps[i].comp);
 				comp.solo = comps[i].solo;
-				comp.enabled = comps[i].mute;
 				comp.audioEnabled = comps[i].mute;
 				if (comp.hasAudio) {
 					comp.audio.audioLevels.setValue(panVolume(comps[i].pan, comps[i].volume * comps[i].gain));
