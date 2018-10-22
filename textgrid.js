f271af27a2306bd0fdd3a4ef10ff66c94f5cbc92
MysteryPancake <MysteryPancake@users.noreply.github.com>
2018-10-22 11:56:11 +1100

    now works with short textgrid
    it's a bit ugly but who cares

--- textgrid.js
+++ textgrid.js
@@ -6,19 +6,28 @@ function textGrid(grid, file) {
 		phones: {}
 	};
 	const lines = grid.split("\n");
-	let size = "2";
+	let intervals = 0;
+	let size = 2;
 	let mode;
 	let prev;
 	while (lines.length) {
 		const line = lines.shift().trim();
-		if (line.startsWith("size")) {
-			size = line.split("=").pop().trim();
-		} else if (line.match(/\[[0-9]+\]:/)) {
-			if (line.startsWith("item")) {
-				mode = line.endsWith("[" + size + "]:") ? "phones" : "words";
-				prev = undefined;
-			} else if (mode && line.startsWith("intervals")) {
-				const xmin = parseFloat(lines.shift().split("=").pop());
+		if (line.endsWith("<exists>")) {
+			size = parseInt(lines.shift().split("=").pop().trim());
+		} else if (line.endsWith("\"IntervalTier\"")) {
+			intervals++;
+			prev = undefined;
+			mode = intervals === size ? "phones" : "words";
+			for (let i = 0; i < 3; i++) {
+				lines.shift();
+			}
+			const count = parseInt(lines.shift().split("=").pop());
+			for (let j = 0; j < count; j++) {
+				let xmin = lines.shift();
+				if (xmin.match(/\[[0-9]+\]:/)) {
+					xmin = lines.shift();
+				}
+				xmin = parseFloat(xmin.split("=").pop());
 				const xmax = parseFloat(lines.shift().split("=").pop());
 				let text = lines.shift().split("=").pop().trim().slice(1, -1);
 				if (mode === "words") {
