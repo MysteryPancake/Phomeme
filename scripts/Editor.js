a6247c7b860288f1130c4515d6242c0d5d322848
MysteryPancake <MysteryPancake@users.noreply.github.com>
2019-04-09 19:15:15 +1000

    editor stuff

--- scripts/Editor.js
+++ scripts/Editor.js
@@ -1,73 +1,208 @@
 "use strict";
 
-//var json;
-var player;
-//var canvas;
-//var sample;
-//var context;
-//var frame = 0;
-var fileList = [];
 var sampleRate = 44100;
-//var requestFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(e) { return window.setTimeout(e, 1000 / 60); };
-//var cancelFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.oCancelAnimationFrame || window.msCancelAnimationFrame || function(id) { window.clearTimeout(id); };
-
-function setup() {
-	addFile({ name: "Untitled Session.phomeme" }); // do proper one later
-	//canvas = document.getElementById("canvas");
-	//context = canvas.getContext("2d", { alpha: false });
-	//window.addEventListener("resize", resize);
-	//window.addEventListener("orientationchange", resize);
-	//resize();
-	//frame = requestFrame(draw);
-	//window.addEventListener("wheel", wheel);
-	/*if (window.ontouchstart) {
-		window.addEventListener("touchstart", clicked);
-		window.addEventListener("touchmove", moved);
-		window.addEventListener("touchend", ended);
-	} else {
-		window.addEventListener("mousedown", clicked);
-		window.addEventListener("mousemove", moved);
-		window.addEventListener("mouseup", ended);
-	}*/
-	/*var audioRequest = new XMLHttpRequest();
-	audioRequest.open("GET", "donkeykong/input.wav", true);
-	audioRequest.responseType = "arraybuffer";
-	audioRequest.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200) {
-			player.decodeAudioData(this.response, function(buffer) {
-				sample = { buffer: buffer, data: buffer.getChannelData(0) };
-			});
-		}
-	};
-	audioRequest.send();
-	var jsonRequest = new XMLHttpRequest();
-	jsonRequest.open("GET", "donkeykong/complete.json", true);
-	jsonRequest.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200) {
-			json = JSON.parse(this.responseText);
-		}
-	};
-	jsonRequest.send();*/
-}
+var fileList = [];
+var activeSession;
+var draggedFile;
+var offset = 0;
+var scale = 40;
+var detail = 6;
+var activeClip;
+var playlist;
+var sideNav;
+var initial;
+var player;
 
 function setupAudio() {
+	if (player) return;
 	player = new (window.AudioContext || window.webkitAudioContext)();
 	sampleRate = player.sampleRate;
 }
 
-function toggle(element) {
-	var dropdown = element.nextSibling.nextSibling;
-	var previous = dropdown.style.display;
-	closeMenus();
-	dropdown.style.display = previous === "block" ? "none" : "block";
+function fileBuffer(file, func) {
+	var reader = new FileReader();
+	reader.onload = function() {
+		player.decodeAudioData(this.result, function(buffer) {
+			func(buffer);
+		});
+	};
+	reader.readAsArrayBuffer(file);
 }
 
-function addFile(file) {
-	var listed = document.createElement("a");
-	listed.draggable = true;
-	listed.innerHTML = file.name;
-	document.getElementById("sidenav").appendChild(listed);
-	fileList.push(file);
+/*function urlBuffer(url, func) {
+	var audioRequest = new XMLHttpRequest();
+	audioRequest.open("GET", url, true);
+	audioRequest.responseType = "arraybuffer";
+	audioRequest.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			player.decodeAudioData(this.response, function(buffer) {
+				func(buffer);
+			});
+		}
+	};
+	audioRequest.send();
+}
+
+function urlJson(url, func) {
+	var jsonRequest = new XMLHttpRequest();
+	jsonRequest.open("GET", url, true);
+	jsonRequest.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status === 200) {
+			func(JSON.parse(this.responseText));
+		}
+	};
+	jsonRequest.send();
+}*/
+
+function setSession(session) {
+	if (activeSession) {
+		for (var i = 0; i < activeSession.trackList.length; i++) {
+			activeSession.trackList[i].elem.style.display = "none";
+		}
+	}
+	for (var j = 0; j < session.trackList.length; j++) {
+		session.trackList[j].elem.style.display = "flex";
+	}
+	activeSession = session;
+	initial.style.display = session.trackList.length ? "none" : "block";
+}
+
+function Session(name) {
+	this.realName = name;
+	this.name = name + ".phomeme";
+	this.type = "application/json";
+	this.trackList = [];
+	setSession(this);
+}
+
+function Clip(clipFile, clipTrack) {
+	this.drag = 0;
+	this.audioData;
+	this.audioBuffer;
+	this.track = clipTrack;
+	this.active = false;
+	this.startPoint = 0;
+	this.duration = 128;
+	this.elem = document.createElement("canvas");
+	this.elem.className = "clip";
+	this.elem.style.left = this.startPoint + "px";
+	this.elem.width = this.duration;
+	this.track.elem.appendChild(this.elem);
+	this.context = this.elem.getContext("2d", { alpha: true });
+	this.updateCanvas = function() {
+		this.duration = scale * (this.audioBuffer.length / sampleRate);
+		this.elem.width = this.duration;
+		this.context.clearRect(0, 0, this.elem.width, this.elem.height);
+		var lines = this.elem.width * detail;
+		this.context.lineWidth = 1;
+		this.context.strokeStyle = "white";
+		this.context.beginPath();
+		for (var k = 0; k < lines; k++) {
+			var x = k / lines * this.elem.width;
+			var y = this.elem.height * 0.5;
+			var index = this.audioData[Math.floor((k / detail + offset) * (sampleRate / scale))];
+			this.context.lineTo(x, y + (index || 0) * y);
+		}
+		this.context.stroke();
+	};
+	this.clicked = function(e) {
+		e.preventDefault();
+		activeClip = this;
+		this.drag = e.pageX - this.startPoint;
+	};
+	this.elem.addEventListener("mousedown", this.clicked.bind(this));
+	this.drawLoading = function() {
+		this.context.fillStyle = "white";
+		this.context.textAlign = "center";
+		this.context.textBaseline = "middle";
+		this.context.font = "16px Arial";
+		this.context.fillText("LOADING...", this.elem.width * 0.5, this.elem.height * 0.5);
+	};
+	this.loadFile = function(file) {
+		setupAudio();
+		this.drawLoading();
+		fileBuffer(file, function(buffer) {
+			this.audioData = buffer.getChannelData(0);
+			this.audioBuffer = buffer;
+			this.updateCanvas();
+		}.bind(this));
+	};
+	this.loadFile(clipFile);
+	this.moveTo = function(track) {
+		this.track.removeClip(this);
+		track.addClip(this);
+		this.track = track;
+		track.elem.appendChild(this.elem);
+	};
+	this.moveStart = function(start) {
+		this.startPoint = start;
+		this.elem.style.left = start + "px";
+	};
+}
+
+function Track() {
+	this.clips = [];
+	this.elem = document.createElement("div");
+	this.elem.className = "track";
+	playlist.appendChild(this.elem);
+	this.addClip = function(clip) {
+		this.clips.push(clip);
+	};
+	this.loadClip = function(file) {
+		var clip = new Clip(file, this);
+		this.addClip(clip);
+		return clip;
+	};
+	this.removeClip = function(clip) {
+		var index = this.clips.indexOf(clip);
+		if (index !== -1) {
+			this.clips.splice(index, 1);
+		}
+	};
+	this.moved = function(e) {
+		if (activeClip && activeClip.track !== this) {
+			e.preventDefault();
+			activeClip.moveTo(this);
+		}
+	};
+	this.elem.addEventListener("mousemove", this.moved.bind(this));
+	this.dragover = function(e) {
+		if (draggedFile) {
+			e.preventDefault();
+			e.dataTransfer.dropEffect = "copy";
+		}
+	};
+	this.elem.addEventListener("dragover", this.dragover.bind(this));
+	this.drop = function(e) {
+		if (draggedFile) {
+			e.preventDefault();
+			var clip = this.loadClip(draggedFile);
+			clip.moveStart(e.offsetX);
+			draggedFile = undefined;
+		}
+	};
+	this.elem.addEventListener("drop", this.drop.bind(this));
+	this.trackIndex = activeSession.trackList.push(this);
+	initial.style.display = "none";
+}
+
+function ListedFile(file) {
+	this.file = file;
+	this.elem = document.createElement("a");
+	this.elem.draggable = true;
+	this.elem.innerHTML = this.file.name;
+	sideNav.appendChild(this.elem);
+	this.elem.addEventListener("dragstart", function() {
+		if (this.file.type.startsWith("audio")) {
+			draggedFile = this.file;
+		}
+	}.bind(this));
+	this.elem.addEventListener("click", function() {
+		if (this.file.trackList) {
+			setSession(this.file);
+		}
+	}.bind(this));
+	fileList.push(this);
 }
 
 function closeMenus() {
@@ -77,10 +212,48 @@ function closeMenus() {
 	}
 }
 
+function moved(e) {
+	if (activeClip) {
+		e.preventDefault();
+		activeClip.moveStart(Math.max(0, e.pageX - activeClip.drag));
+	}
+}
+
+function ended() {
+	activeClip = undefined;
+}
+
+function setup() {
+	playlist = document.getElementById("playlist");
+	initial = document.getElementById("initial");
+	sideNav = document.getElementById("sidenav");
+	window.addEventListener("mousemove", moved);
+	window.addEventListener("mouseup", ended);
+	new ListedFile(new Session("Untitled Session"));
+	/*urlBuffer("donkeykong/input.wav", function(buffer) {
+		sample = { buffer: buffer, data: buffer.getChannelData(0) };
+	});
+	urlJson("donkeykong/complete.json", function(response) {
+		json = response;
+	});*/
+	window.addEventListener("click", function(e) {
+		if (!e.target.matches(".dropbutton")) {
+			closeMenus();
+		}
+	});
+}
+
+function toggle(element) {
+	var dropdown = element.nextSibling.nextSibling;
+	var previous = dropdown.style.display;
+	closeMenus();
+	dropdown.style.display = previous === "block" ? "none" : "block";
+}
+
 function newSession() {
 	var sessionName = window.prompt("Please enter a session name", "Untitled Session");
 	if (sessionName) {
-		addFile({ name: sessionName + ".phomeme" }); // make this a proper file later
+		new ListedFile(new Session(sessionName));
 	}
 }
 
@@ -89,38 +262,22 @@ function loadSession(element) {
 }
 
 function importFile(element) {
-	var file = element.files[0];
-	if (file) {
-		addFile(file);
+	for (var i = 0; i < element.files.length; i++) {
+		var file = element.files[i];
+		new ListedFile(file);
+		if (file.type.startsWith("audio")) {
+			var track = new Track();
+			track.loadClip(file);
+		}
 	}
 }
 
-window.onclick = function(e) {
-	if (!e.target.matches(".dropbutton")) {
-		closeMenus();
-	}
-}
-
-/*function resize() {
-	cancelFrame(frame);
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	frame = requestFrame(draw);
-}
-
-function drawLine(context, x, y, x2, y2) {
+/*function drawLine(context, x, y, x2, y2) {
 	context.beginPath();
 	context.moveTo(x, y);
 	context.lineTo(x2, y2);
 	context.stroke();
 }
-
-var offset = 0;
-var scale = 500;
-var yPos = 64;
-var height = 256;
-var scrollHeight = 64;
-var dragging = false;
 
 function drawBoxes(json, audio) {
 	if (json) {
@@ -159,18 +316,7 @@ function drawBoxes(json, audio) {
 		}
 	}
 	if (audio) {
-		var detail = 2;
-		var lines = canvas.width * detail;
-		context.lineWidth = 1;
-		context.strokeStyle = "white";
-		context.beginPath();
-		for (var k = 0; k < lines; k++) {
-			var x = k / lines * canvas.width;
-			var y = yPos + height * 0.5;
-			var index = audio[Math.floor((k / detail + offset) * (sampleRate / scale))];
-			context.lineTo(x, y + 0.5 * height * (index || 0));
-		}
-		context.stroke();
+		COPIED UP THERE ALREADY
 	}
 	context.lineWidth = 2;
 	context.strokeStyle = "#00FF00";
@@ -203,7 +349,7 @@ function drawScroll(audio) {
 
 function clamp(offset) {
 	if (sample) {
-		return Math.min(Math.max(offset, 0), sample.data.length * scale / sampleRate - canvas.width);
+		return Math.min(Math.max(offset, 0), sample.buffer.length * scale / sampleRate - canvas.width);
 	} else {
 		return Math.max(offset, 0);
 	}
@@ -246,14 +392,14 @@ function clicked(e) {
 		}
 	} else if (e.pageY < scrollHeight) {
 		if (!sample) return;
-		var start = (offset * sampleRate * canvas.width) / (sample.data.length * scale);
-		var end = (canvas.width * sampleRate * canvas.width) / (sample.data.length * scale);
+		var start = (offset * sampleRate * canvas.width) / (sample.buffer.length * scale);
+		var end = (canvas.width * sampleRate * canvas.width) / (sample.buffer.length * scale);
 		if (near(e.pageX, start)) {
 			dragging = { initial: offset, direction: true };
 		} else if (near(e.pageX, start + end)) {
 			dragging = { initial: offset, direction: false };
 		} else {
-			offset = clamp((e.pageX * sample.data.length * scale) / (sampleRate * canvas.width) - (0.5 * canvas.width));
+			offset = clamp((e.pageX * sample.buffer.length * scale) / (sampleRate * canvas.width) - (0.5 * canvas.width));
 			canvas.style.cursor = "grabbing";
 			dragging = true;
 		}
@@ -280,8 +426,8 @@ function moved(e) {
 		canvas.style.cursor = hovering ? "col-resize" : "auto";
 	} else if (!dragging && e.pageY < scrollHeight) {
 		if (!sample) return;
-		var start = (offset * sampleRate * canvas.width) / (sample.data.length * scale);
-		var end = (sampleRate * canvas.width * canvas.width) / (sample.data.length * scale);
+		var start = (offset * sampleRate * canvas.width) / (sample.buffer.length * scale);
+		var end = (sampleRate * canvas.width * canvas.width) / (sample.buffer.length * scale);
 		if (near(e.pageX, start) || near(e.pageX, start + end)) {
 			canvas.style.cursor = "col-resize";
 		} else {
@@ -297,14 +443,14 @@ function moved(e) {
 		} else if (dragging.direction !== undefined) {
 			if (dragging.direction) {
 				//offset = (offset / dragging.initial) * sampleRate;
-				//scale = (sampleRate * canvas.width * canvas.width) / (sample.data.length * e.pageX); // todo
-				//offset = clamp((e.pageX * sample.data.length * scale) / (sampleRate * canvas.width));
+				//scale = (sampleRate * canvas.width * canvas.width) / (sample.buffer.length * e.pageX); // todo
+				//offset = clamp((e.pageX * sample.buffer.length * scale) / (sampleRate * canvas.width));
 			} else {
-				//scale = (sampleRate * canvas.width * canvas.width) / (sample.data.length * e.pageX);
+				//scale = (sampleRate * canvas.width * canvas.width) / (sample.buffer.length * e.pageX);
 				//offset = (dragging.initial / offset); // todo
 			}
 		} else {
-			offset = clamp((e.pageX * sample.data.length * scale) / (sampleRate * canvas.width) - (0.5 * canvas.width));
+			offset = clamp((e.pageX * sample.buffer.length * scale) / (sampleRate * canvas.width) - (0.5 * canvas.width));
 			canvas.style.cursor = "grabbing";
 		}
 	}
@@ -319,7 +465,7 @@ function ended(e) {
 function draw() {
 	context.fillStyle = "#061306";
 	context.fillRect(0, 0, canvas.width, canvas.height);
-	drawBoxes(json, sample && sample.data);
-	drawScroll(sample && sample.data);
+	drawBoxes(json, sample && sample.buffer);
+	drawScroll(sample && sample.buffer);
 	frame = requestFrame(draw);
 }*/
